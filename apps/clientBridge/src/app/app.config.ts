b730b8d5923a51e3c58a1ed0ab87e29522a99f2d
Aria <ariaman2002@gmail.com>
2025-01-21 23:24:56 +0330

    Create alert library, use matSnackBar and provide it in app.config.ts

--- apps/clientBridge/src/app/app.config.ts
+++ apps/clientBridge/src/app/app.config.ts
@@ -1,10 +1,12 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideSharedUiAlert } from '@insurance-shared-ui-alert';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    provideSharedUiAlert(),
   ],
 };

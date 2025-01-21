fb01ad6a84bc130727700e850b048085538b6152
Aria <ariaman2002@gmail.com>
2025-01-22 02:15:43 +0330

    Add themes in project for dark mode and light mode, another themes.

--- apps/brokerAdmin/src/app/app.config.ts
+++ apps/brokerAdmin/src/app/app.config.ts
@@ -2,11 +2,15 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideSharedUiAlert } from '@insurance-shared-ui-alert';
+import { provideSharedDataSetting } from '@insurance-shared-data-setting';
+import { MAT_DEFAULT_OPTIONS_OVERRIDES } from '@insurance-shared-util-web-sdk';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
     provideSharedUiAlert(),
+    provideSharedDataSetting(),
+    ...MAT_DEFAULT_OPTIONS_OVERRIDES,
   ],
 };

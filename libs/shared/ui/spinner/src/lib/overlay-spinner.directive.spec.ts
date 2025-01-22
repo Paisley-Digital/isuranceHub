9deeea11e241f31e9345a9b3f4cf654d56e08861
Aria <ariaman2002@gmail.com>
2025-01-22 23:11:47 +0330

    Create spinner library for set spinner loading in elements.

---
+++ libs/shared/ui/spinner/src/lib/overlay-spinner.directive.spec.ts
@@ -0,0 +1,8 @@
+import { OverlaySpinnerDirective } from './overlay-spinner.directive';
+
+describe('OverlaySpinnerDirective', () => {
+  it('should create an instance', () => {
+    const directive = new OverlaySpinnerDirective();
+    expect(directive).toBeTruthy();
+  });
+});
